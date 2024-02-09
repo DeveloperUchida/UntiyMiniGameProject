8970e8e46658a0c0273eec68dae5071df6246903
DeveloperUchida <kouei28poiou@icloud.com>
2024-02-09 22:37:17 +0900

    DataUpdate 2024/02/09

--- Assets/Scripts/GaneSystemScripts.cs
+++ Assets/Scripts/GaneSystemScripts.cs
@@ -4,11 +4,13 @@ public class GaneSystemScripts : MonoBehaviour
 {
     //GamePlayerMotionFunction
     public float PlayerControlSpeed; // Speed Control use UnityEditor
+    private Rigidbody2D rb; //RigidBody2DDeclatation
 
+    public float jumpPower; //PrivateJumpPowerDeclatation
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,7 +19,20 @@ public class GaneSystemScripts : MonoBehaviour
         //GamePlayerControlCode
         if(Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0,PlayerControlSpeed,0);
+            transform.Translate(0,PlayerControlSpeed,0); //ForwardKey
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(PlayerControlSpeed,0,0); //LeftKey
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0,-PlayerControlSpeed,0); //BackKey
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(0,0,0); //SpaceJumpKey
+            rb.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
         }
     }
 }
